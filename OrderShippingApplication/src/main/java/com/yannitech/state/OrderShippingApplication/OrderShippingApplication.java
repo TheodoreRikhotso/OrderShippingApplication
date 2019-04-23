1355372ea3923e6fb8137d0fb9113a55eb139dcf
TheodoreRikhotso <rikhotsothoko@gmail.com>
2019-04-23 10:51:26 +0200

    upload project

---
+++ OrderShippingApplication/src/main/java/com/yannitech/state/OrderShippingApplication/OrderShippingApplication.java
@@ -0,0 +1,13 @@
+package com.yannitech.state.OrderShippingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderShippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderShippingApplication.class, args);
+	}
+
+}
